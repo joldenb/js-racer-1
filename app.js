ecc3a99979eb48fae323acdacdb9c9c856935fbe
katcozadd <krosecozadd@gmail.com>
2018-03-24 14:26:25 -0600

    Commit 3 - Created an event listener for car 1

---
+++ app.js
@@ -0,0 +1,7 @@
+//Selecting element: car-one
+let carOne = document.getElementById('box-one');
+
+//Adding event listener to car-one
+carOne.addEventListener('keydown' ,(event) => {
+	alert('clicked box');
+})
