33e41beabce5c215cbef8dd74aafc4bcefc85f05
katcozadd <krosecozadd@gmail.com>
2018-03-24 21:26:38 -0600

    Commit 5 - Created functions that move one div or the other div

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-//Selecting element: car-one
+//Selecting element by ID: 'box-one' and 'box-two' 
 let carOne = document.getElementById('box-one');
+let carTwo = document.getElementById('box-two');
 
+//Defining two variables that start at 0px
+let carOneLeft = 0;
+let carTwoLeft = 0;
 
-//Adding event listener to car-one
+const carOneMove = (event) => {
 
+	//creating an if statement to detect which key is being pushed down with the event.keyCode
+	//event.keyCode references to the right arrow key being pressed
+	if (event.keyCode === 39) {
+	//taking initial value of carOneLeft at 0 and adding 10px 
+		carOneLeft += 10;
+	//selecting the carOne object and apply style.left allows carOneLeft to concatenate the 10px from previous line	and move 10px away from the left
+		carOne.style.left = carOneLeft + "px";
+	}
+};
+//Calling the carOneMove function everytime the right arrow key is pressed down
+document.onkeydown = carOneMove;
+
+const carTwoMove = (event) => {
+
+	//creating an if statement to detect which key is being pushed down with the event.keyCode
+	//event.keyCode references to the 'q' key being pressed
+	if (event.keyCode === 81) {
+	//taking initial value of carTwoLeft at 0 and adding 10px 
+		carTwoLeft += 10;
+	//selecting the carTwo object and apply style.left allows carTwoLeft to concatenate the 10px from previous line	and move 10px away from the left
+		carTwo.style.left = carTwoLeft + "px";
+	}
+};
+//Calling the carTwoMove function everytime the 'q' key is pressed down
+document.onkeydown = carTwoMove;
+
+/*
+
+window.addEventListener('keydown', checkKeyPressed, false);
 
 const checkKeyPressed = (key) => {
 	if (key.keyCode == '75') {
 		alert("The 'k' letter key has been pressed!");
 	}
 };
-
-window.addEventListener('keydown', checkKeyPressed, false);
-/*
 
 
 	
@@ -23,12 +53,5 @@ carOne.addEventListener('click', () => {
 	alert("shit was clicked");
 })
 
-	if (event.keyCode === 75) {
-		alert("Key 'k' was pressed down");
-
-if (keyName === 'k') {
-		alert('Key k was pressed down');
-	}
-})
 
 */
