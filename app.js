c4e76f1e12169d8ebccdcb67cb2488753c940a8b
katcozadd <krosecozadd@gmail.com>
2018-03-25 13:52:22 -0600

    Commit 7 - Created function that moves both divs, a container div and more styling

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let carOneLeft = 0;
 let carTwoLeft = 0;
 
 
-const carOneMove = (event) => {
+const goatsGo = (event) => {
 
 	//creating an if statement to detect which key is being pushed down with the event.keyCode
 	//event.keyCode references to the right arrow key being pressed
@@ -17,12 +17,35 @@ const carOneMove = (event) => {
 	//selecting the carOne object and apply style.left allows carOneLeft to concatenate the 10px from previous line	and move 10px away from the left
 		carOne.style.left = carOneLeft + "px";
 	}
+	//creating an if statement to detect which key is being pushed down with the event.keyCode
+	//event.keyCode references to the 'q' key being pressed
+	if (event.keyCode === 81) {
+	//taking initial value of carTwoLeft at 0 and adding 10px 
+		carTwoLeft += 10;
+	//selecting the carTwo object and apply style.left allows carTwoLeft to concatenate the 10px from previous line	and move 10px away from the left
+		carTwo.style.left = carTwoLeft + "px";
+	}
 };
 //Calling the carOneMove function everytime the right arrow key is pressed down
-document.onkeydown = carOneMove;
-carOneMove();
+document.onkeydown = goatsGo;
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
+
+//These are other things that I tried that I am not sure if I want to get rid of yet
+
+//Copy of second function
 const carTwoMove = (event) => {
 
 	//creating an if statement to detect which key is being pushed down with the event.keyCode
@@ -36,20 +59,7 @@ const carTwoMove = (event) => {
 };
 //Calling the carTwoMove function everytime the 'q' key is pressed down
 document.onkeydown = carTwoMove;
-carTwoMove();
 
-*/
-
-
-
-
-
-
-
-
-/*
-
-//These are other things that I tried that I am not sure if I want to get rid of yet
 
 window.addEventListener('keydown', checkKeyPressed, false);
 
