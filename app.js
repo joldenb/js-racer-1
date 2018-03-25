2892193ea190110b228d5e5c7e0a4dd41b264bcc
katcozadd <krosecozadd@gmail.com>
2018-03-24 18:26:42 -0600

    Commit 4 - Created event listener that alerts browser when the 'k' key is being pressed down

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 //Selecting element: car-one
 let carOne = document.getElementById('box-one');
 
+
 //Adding event listener to car-one
-carOne.addEventListener('keydown' ,(event) => {
-	alert('clicked box');
+
+
+const checkKeyPressed = (key) => {
+	if (key.keyCode == '75') {
+		alert("The 'k' letter key has been pressed!");
+	}
+};
+
+window.addEventListener('keydown', checkKeyPressed, false);
+/*
+
+
+	
+//These are other things that I tried that I am still trying to work on
+
+//I used this to test that I selected element correctly
+carOne.addEventListener('click', () => {
+	alert("shit was clicked");
 })
+
+	if (event.keyCode === 75) {
+		alert("Key 'k' was pressed down");
+
+if (keyName === 'k') {
+		alert('Key k was pressed down');
+	}
+})
+
+*/
